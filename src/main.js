b097b167eb481972087407c33197c2c123352349
Anthony <anthonymaia00@gmail.com>
2023-10-18 19:45:53 -0300

    Criando o cabeçalho

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
-    
+
+// animação do header 
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHeroSection = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const alturaScrollY = window.scrollY;
+
+        if (alturaScrollY < alturaHeroSection) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    // seção de shows ou atraçÕes
+
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,10 +30,31 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+
+    // seção de FAQ (accordion)
+
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', closeOrOpen);
     }
 })
+
+// função para ocultar elementos do header
+
+const ocultaElementosDoHeader = () => {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+// função para exiber elementos do header
+
+const exibeElementosDoHeader = () => {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+
+
+// função para abrir e fechar o FAQ
 
 const closeOrOpen = (elemento) => {
     const classe = 'faq__questions__item--is-open';
@@ -25,6 +63,8 @@ const closeOrOpen = (elemento) => {
     elementoPai.classList.toggle(classe);
 }
 
+//função para remover a classe --is-active do botão 
+
 const removeBotaoAtivo = () => {
     const buttons = document.querySelectorAll('[data-tab-button]');
     
@@ -32,6 +72,8 @@ const removeBotaoAtivo = () => {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
+
+// função para remover a classe --is-active dos tabs
 
 const escondeTodasAbas = () => {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
